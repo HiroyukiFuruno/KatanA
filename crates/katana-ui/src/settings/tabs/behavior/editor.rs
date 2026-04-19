179d15e7461ccf9f38f335776837011d79e0e1db
HiroyukiFuruno <hfuruno0114@gmail.com>
2026-04-20 07:35:22 +0900

    refactor: Behavior設定モジュールのファイル分割（AST Linterエラー解消）

--- crates/katana-ui/src/settings/tabs/behavior/editor.rs
+++ crates/katana-ui/src/settings/tabs/behavior/editor.rs
@@ -1,12 +1,11 @@
-use super::types::*;
-use crate::app_state::AppAction;
+use crate::app_state::AppState;
 use crate::settings::*;
+use eframe::egui;
 
-impl BehaviorTabOps {
-    pub(crate) fn render_behavior_tab(
-        ui: &mut egui::Ui,
-        state: &mut crate::app_state::AppState,
-    ) -> Option<AppAction> {
+pub struct BehaviorEditorOps;
+
+impl BehaviorEditorOps {
+    pub(super) fn render(ui: &mut egui::Ui, state: &mut AppState) {
         let behavior_msgs = &crate::i18n::I18nOps::get().settings.behavior;
 
         let mut confirm = state
@@ -126,75 +125,6 @@ impl BehaviorTabOps {
 
             ui.spacing_mut().slider_width = original_width;
         }
-
-        ui.add_space(SUBSECTION_SPACING);
-
-        let preview_msgs = &crate::i18n::I18nOps::get().preview;
-        ui.label(egui::RichText::new(&preview_msgs.slideshow_settings).strong());
-        ui.add_space(SETTINGS_TOGGLE_SPACING);
-
-        let mut slideshow_hover = state
-            .config
-            .settings
-            .settings()
-            .behavior
-            .slideshow_hover_highlight;
-        if ui
-            .add(
-                crate::widgets::LabeledToggle::new(
-                    &preview_msgs.highlight_hover,
-                    &mut slideshow_hover,
-                )
-                .position(crate::widgets::TogglePosition::Right)
-                .alignment(crate::widgets::ToggleAlignment::SpaceBetween),
-            )
-            .changed()
-        {
-            state
-                .config
-                .settings
-                .settings_mut()
-                .behavior
-                .slideshow_hover_highlight = slideshow_hover;
-            state.layout.slideshow_hover_highlight = slideshow_hover;
-            let _ = state.config.try_save_settings();
-        }
-        ui.add_space(SETTINGS_TOGGLE_SPACING);
-
-        let mut auto_hide = state
-            .config
-            .settings
-            .settings()
-            .behavior
-            .slideshow_show_diagram_controls;
-        if ui
-            .add(
-                crate::widgets::LabeledToggle::new(
-                    &preview_msgs.show_diagram_controls,
-                    &mut auto_hide,
-                )
-                .position(crate::widgets::TogglePosition::Right)
-                .alignment(crate::widgets::ToggleAlignment::SpaceBetween),
-            )
-            .changed()
-        {
-            state
-                .config
-                .settings
-                .settings_mut()
-                .behavior
-                .slideshow_show_diagram_controls = auto_hide;
-            state.layout.slideshow_show_diagram_controls = auto_hide;
-            let _ = state.config.try_save_settings();
-        }
-
-        ui.add_space(SUBSECTION_SPACING);
-
-        if ui.button(&behavior_msgs.clear_http_cache).clicked() {
-            return Some(AppAction::ClearAllCaches);
-        }
-
-        None
     }
 }
 
